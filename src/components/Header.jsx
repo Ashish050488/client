ac806a6218b1afd7130b5c5ec27595cd8390581b
Ashish050488 <ashishar050488@gmail.com>
2024-08-23 20:29:55 +0530

    front-end part-complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../images/logo.png'
 import { AiOutlineClose } from "react-icons/ai";
 import { FaBars } from "react-icons/fa";
 
 const Header = () => {
+  const [isNavShowing,setIsNavShowing]=useState(window.innerWidth>800?true:false);
+  const closeNavHandler = ()=>{
+    if(window.innerWidth<800){
+      setIsNavShowing(false)
+    }else{
+      setIsNavShowing(true)
+    }
+  }
   return (
     <nav>
       <div className="container nav_container">
         <Link to='/' className='nav_logo'>
           <img src={logo} alt="" />
         </Link>
-        <ul className='nav_menu'>
-            <li><Link to={'/profile/:id'}>Ashish</Link></li>
-            <li><Link to={'/create'}>Create Post</Link></li>
-            <li><Link to={'/authors'}>Authors</Link></li>
-            <li><Link to={'/logout'}>Logout</Link></li>
-            <button className='nav_toggle-btn'>
-            <AiOutlineClose />
+        { isNavShowing && <ul className='nav_menu'>
+            <li><Link to={'/profile/:id'} onClick={closeNavHandler}>Ashish</Link></li>
+            <li><Link to={'/create'} onClick={closeNavHandler}>Create Post</Link></li>
+            <li><Link to={'/authors'} onClick={closeNavHandler}>Authors</Link></li>
+            <li><Link to={'/logout'} onClick={closeNavHandler}>Logout</Link></li>
+        </ul>}
+            <button className='nav_toggle-btn' onClick={()=>setIsNavShowing(!isNavShowing)}>
+            {isNavShowing?<AiOutlineClose />:<FaBars/>}
             </button>
-        </ul>
       </div>
     </nav>
   )
